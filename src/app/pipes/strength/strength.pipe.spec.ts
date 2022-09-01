0e72dab323cbd7582c8544d06363a7b2ab16dc4e
BeshoyStorexweb <Beshoy.f@storexweb.com>
2022-09-01 17:45:17 +0200

    finish add pipe

--- src/app/pipes/strength/strength.pipe.spec.ts
+++ src/app/pipes/strength/strength.pipe.spec.ts
@@ -5,4 +5,13 @@ describe('StrengthPipe', () => {
     const pipe = new StrengthPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('Should display weak if value is less than 10', () => {
+    const pipe = new StrengthPipe();
+    expect(pipe.transform(5)).toEqual('5 is Weak');
+  });
+  it('Should display weak if value is less than 30', () => {
+    const pipe = new StrengthPipe();
+    expect(pipe.transform(30)).toEqual('30 is Strongest');
+  });
 });
